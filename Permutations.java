baa215ec3701bf11e2d98e2cf52d5cdcd5123d78
DevanshiGarg08 <57318946+DevanshiGarg08@users.noreply.github.com>
2020-01-25 16:28:02 +0530

    Permutations of a String

---
+++ Permutations.java
@@ -0,0 +1,40 @@
+
+package practice2;
+
+import java.util.Scanner;
+
+public class Permutations
+{
+     static void permute(String s,int start,int end)
+    {
+        if(start==end)
+        {
+            System.out.println(s);
+        }
+        else
+        {
+            for(int i=start;i<=end;i++)
+            {
+                s=swap(s,start,i);
+                permute(s,start+1,end);
+                s=swap(s,start,i);
+            }
+        }
+    }
+    static String swap(String s,int i,int j)
+    {
+        char temp;
+        char[] ch=s.toCharArray();
+        temp=ch[i];
+        ch[i]=ch[j];
+        ch[j]=temp;
+        return String.valueOf(ch);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int l=s.length();
+        permute(s,0,l-1);
+
+    }
+}
